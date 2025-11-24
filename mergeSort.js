ea80e3ced8c710a4618ef54969a036c56f102ec7
shradhanand patil <user@example.com>
2025-11-24 15:41:26 +0530

    Implement Merge Sort algorithm for sorting an array

---
+++ mergeSort.js
@@ -0,0 +1,26 @@
+var sortArray = function (nums) {
+  if (nums.length <= 1) return nums;
+  let mid = Math.floor(nums.length / 2);
+  let left = sortArray(nums.slice(0, mid));
+  let right = sortArray(nums.slice(mid));
+  return merge(left, right);
+};
+fanion;
+function merge(left, right) {
+  let i = 0;
+  let j = 0;
+  let res = [];
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
+    }
+  }
+
+  return [...res, ...left.slice(i), ...right.slice(j)];
+}
+
+console.log(sortArray([3, 6, 9, 2, 1]));
