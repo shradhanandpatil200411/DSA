f2cc284ea6cdce68df85dbbe2a28f3b908a075ef
shradhanand Patil <patilshradhanand@gmail.com>
2026-03-09 22:53:09 +0530

    find the second largest number

---
+++ Array/findSecondLargest.js
@@ -0,0 +1,15 @@
+function findSecondLargest(arr) {
+  let fMax = Math.max(arr[0], arr[1]);
+  let sMax = Math.min(arr[0], arr[1]);
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] > fMax) {
+      sMax = fMax;
+      fMax = arr[i];
+    } else if (sMax < arr[i] && arr[i] != fMax) {
+      sMax = arr[i];
+    }
+  }
+  console.log(sMax);
+}
+
+findSecondLargest([1, 2, 3, 5, 6, 6, 7, 7]);
